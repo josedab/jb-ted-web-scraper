f4f0d5d938add6c5d18cd5313d7b8027dafb6800
Jose David Baena <josedab@gmail.com>
2015-02-28 01:55:54 +0000

    Related videos extractor now creates absolute urls based on relative ones - Fixes #7

--- src/main/java/com/technobitia/ted/examples/RelatedVideosExtractorApp.java
+++ src/main/java/com/technobitia/ted/examples/RelatedVideosExtractorApp.java
@@ -12,7 +12,7 @@ public class RelatedVideosExtractorApp {
     public static void main(String[] args) throws TedScraperException {
         String url = "http://www.ted.com/talks/rob_knight_how_our_microbes_make_us_who_we_are";
         TedScraperRequest request = new TedScraperRequest.Builder(url)
-                                                        .build();
+                                                          .build();
         TedScraperClient client = new TedScraperClient();
         List<RelatedVideo> relatedVideos = client.extractRelatedVideos(request);
         System.out.println(relatedVideos);
